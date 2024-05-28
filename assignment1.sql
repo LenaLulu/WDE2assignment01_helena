b4b4880422f4abf1f121f06705d53f28680395b8
LenaLulu <ion.asfaw@gmail.com>
2024-05-28 10:57:16 -0400

    First commit for WDE2 assignment 01

---
+++ assignment1.sql
@@ -0,0 +1,48 @@
+CREATE TABLE Clients{
+    cid INTEGER PRIMARY KEY
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    address TEXT,
+    phone INTEGER,
+    age INTEGER 
+};
+
+INSERT INTO Clients (cid, first_name, last_name, address, phone, age)
+    VALUES (1, John, Smith, 123 The Boulevard, 4159985698, 42);
+
+INSERT INTO Clients (cid, first_name, last_name, address, phone, age)
+    VALUES (2, Jane, Smith, 123 The Boulevard, 4156523541, 45);
+
+INSERT INTO Clients (cid, first_name, last_name, address, phone, age)
+    VALUES (3, Oliver, Twist, 235 Yorkshire Ave., 3352639875, 13);
+
+INSERT INTO Clients (cid, first_name, last_name, address, phone, age)
+    VALUES (4, Hyacinthe, Devers, 7756 Fort York St., 4167356452, 26);
+
+INSERT INTO Clients (cid, first_name, last_name, address, phone, age)
+    VALUES (5, Cebille, Despardieux, 5696 Blvd Orleans, 5639867856, 33);
+
+INSERT INTO Clients (cid, first_name, last_name, address, phone, age)
+    VALUES (6, Jenner, Darcy, 659 Oakland crt, 4639856325, jdarcy@jennerdarcy.com);
+
+
+-- MAX() & MIN() functions return the maximum or minimum values in a set of values.
+
+SELECT MAX(age)
+FROM Clients;
+--returns the maximum age in the Clients table.
+
+
+-- COUNT() function returns the number of records returned by a select query. 
+
+SELECT COUNT(cid)
+FROM Clients;
+--returns the number of Clients in the Clients table. 
+
+-- TRIM() function removes leading and trailing spaces from a string.
+
+SELECT TRIM(first_name, last_name)
+FROM Clients;
+--returns the trimmed version of the first and last names of clients from the Clients table. 
+
+
---
+++ assignment1readme.md
@@ -0,0 +1,32 @@
+MAX() FUNCTION
+
+Definition & Usage:
+The MAX() function is used to return the maximum value in a set of values.  It is an aggregate function, which means it is often used in conjunction with the GROUP BY clause to return the maximum value of each group, or on its own to return the maximum value of an entire dataset.
+
+Syntax:
+MAX()
+
+Parameter Values:
+Expression --> Required. A numeric value(a field or formula)
+
+COUNT() FUNCTION 
+
+Definition & Usage:
+COUNT() function returns the number of rows that match a specified condition. It is used to count rows in a table or to count non-null values in a specified column.
+
+Syntax:
+COUNT()
+
+Parameter Values:
+Expression --> Required. A field or a string value
+
+TRIM() FUNCTION
+
+Definition & Usage:
+The TRIM() function is used to remove specified prefixes or suffixes (whitespace by default) from a string. This function can be particularly useful for cleaning up data by removing unwanted spaces or other characters from the beginning, end, or both ends of a string.
+
+Syntax:
+TRIM()
+
+Parameter Values:
+String --> Required. The string that needs to have empty spaces removed. 
